bbc086dc3b8f2459434b1fbb10cede4ae27faa65
shrey07052010 <118438911+shrey07052010@users.noreply.github.com>
2023-05-05 15:48:16 +0530

    Add files via upload

---
+++ project 100/chat_room.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAvOc7RZQ74ufP94zsOV6-EuB1v9hghhUw",
+    authDomain: "project-100-1132a.firebaseapp.com",
+    databaseURL: "https://project-100-1132a-default-rtdb.firebaseio.com",
+    projectId: "project-100-1132a",
+    storageBucket: "project-100-1132a.appspot.com",
+    messagingSenderId: "1049613551160",
+    appId: "1:1049613551160:web:09903c614e6248c953c8cd"
+  };
+
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
